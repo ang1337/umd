6123c7f3143e8aac7396d248626217acfa6a4cf9
ang1337 <segfault0x00@yahoo.com>
2021-02-06 15:22:21 +0200

    Update api_usage_example.cpp

--- api_usage_example.cpp
+++ api_usage_example.cpp
@@ -8,11 +8,32 @@ int main(int argc, char **argv) {
         printf("Usage: %s <target PID>\n", argv[0]);
         exit(1);
     }
+
     try {
         umd::Dumper dump_obj(std::stoi(argv[1]));
+
         const auto res { dump_obj.dump_memory() };
+
         dump_obj.dump_to_disk(res, "/home/user/test_dump.umd");
+
+        auto mem_chunk { dump_obj.inspect_memory(res, 0xdeadbeefcafebabe, 1337) };
+
+        if (mem_chunk.has_value()) {
+            std::cout << "Successfully read 1337 bytes from address 0xdeadbeefcafebabe:\n";
+
+            for (const auto byte : *mem_chunk) {
+                std::cout << byte << ' ';
+            }
+
+            std::cout << std::endl;
+
+        } else {
+            std::cerr << "Cannot read 1337 bytes from address 0xdeadbeefcafebabe\n";
+        }
+
     } catch (const umd::umd_exception &e) {
         std::cerr << e.what() << std::endl;
     }
+
+    return 0;
 }
